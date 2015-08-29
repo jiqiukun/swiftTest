c0739f2eb23f85145683f3245a851adc33dba670
kyleabby <Tung@jqk>
2015-08-29 09:38:37 +0800

    Initial Commit

---
+++ DuDu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuDu
+//
+//  Created by Kyle on 15/8/29.
+//  Copyright © 2015年 kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
